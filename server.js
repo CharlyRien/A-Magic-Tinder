7e643327e93099ce459d4833e4991cfae4987718
Charles-Eddy Parpet <charly-9-94@hotmail.fr>
2015-11-13 11:24:01 +0100

    correctif bug server

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/www'));
 app.use('/api', api);
 
 process.on('uncaughtException', function (err) {
